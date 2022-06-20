89e923431a2347e733fb433dd7e86fdc66be914d
Viktor <viktor@apilab.ru>
2022-06-20 12:18:58 +0300

    make lib with interfaces

---
+++ libs/api-interfaces/src/map/difficulty.ts
@@ -0,0 +1,15 @@
+export enum Difficulty {
+  expertPlus = 'expertPlus',
+  expert = 'expert',
+  hard = 'hard',
+  normal = 'normal',
+  easy = 'easy',
+}
+
+export const DIFFICULTY_MAP = {
+  'ExpertPlus': Difficulty.expertPlus,
+  'Expert': Difficulty.expert,
+  'Hard': Difficulty.hard,
+  'Normal': Difficulty.normal,
+  'Easy': Difficulty.easy,
+};
---
+++ libs/api-interfaces/src/map/map.ts
@@ -0,0 +1,51 @@
+import { Difficulty } from "./difficulty";
+
+export interface IMap {
+  id: string;
+  coverURL: string;
+  songName: string;
+  songSubName: string;
+  songAuthorName: string;
+  author: string;
+  bpm: number;
+  createdAt: string;
+
+  songFilename: string;
+  difsDetails: MapDiffiDetail[];
+  mods: MapMode[];
+
+  duration: number;
+  difficultMap: MapDifficultList[];
+}
+
+export interface MapDiffiDetail {
+  difficulty: Difficulty;
+  nps: number;
+}
+
+export enum MapMode {
+  degree90 = 'degree90',
+  degree360 = 'degree360',
+  lawless ='lawless',
+  lightShow = 'lightShow',
+  noArrows = 'noArrows',
+  oneSaber = 'oneSaber',
+  standard = 'standard',
+}
+
+export interface MapDifficultList {
+  mode: MapMode;
+  list: MapDifficultInfo[];
+}
+
+export interface MapDifficultInfo {
+  difficulty: Difficulty;
+  noteJumpMovementSpeed: number;
+  noteJumpStartBeatOffset: number;
+  file: string;
+  obstacleColor?: {
+    r: number;
+    g: number;
+    b: number;
+  }
+}
